723fb4dcef6afd4ae7a6b240f45d24d153dde468
SwiftBot Assets <assets@swiftbot.dev>
2026-06-10 23:30:45 +0000

    Auto-build: Update backgrounds from assets.json [skip ci]

---
+++ plugins/commands/economy/assets.js
@@ -0,0 +1,212 @@
+// AUTO-GENERATED - DO NOT EDIT
+// Run GitHub Action to rebuild
+
+export default {
+  "cyber": {
+    "id": "cyber",
+    "name": "Cyber",
+    "type": "static",
+    "price": 5000,
+    "glow": "#9600ff",
+    "tier": "common",
+    "videoUrl": null
+  },
+  "fire": {
+    "id": "fire",
+    "name": "Fire",
+    "type": "static",
+    "price": 10000,
+    "glow": "#ff3200",
+    "tier": "rare",
+    "videoUrl": null
+  },
+  "water": {
+    "id": "water",
+    "name": "Water",
+    "type": "animated",
+    "price": 80000,
+    "glow": "#00b4ff",
+    "tier": "epic",
+    "videoUrl": "https://www.pexels.com/download/video/8249130/"
+  },
+  "hacker": {
+    "id": "hacker",
+    "name": "Hacker",
+    "type": "static",
+    "price": 0,
+    "glow": "#00ff00",
+    "tier": "common",
+    "videoUrl": null
+  },
+  "robot": {
+    "id": "robot",
+    "name": "Robot",
+    "type": "static",
+    "price": 15000,
+    "glow": "#0099ff",
+    "tier": "rare",
+    "videoUrl": null
+  },
+  "matrix": {
+    "id": "matrix",
+    "name": "Matrix",
+    "type": "animated",
+    "price": 120000,
+    "glow": "#00ff00",
+    "tier": "legendary",
+    "videoUrl": "https://videos.pexels.com/video-files/3129671/3129671-hd_1920_1080_30fps.mp4"
+  },
+  "blue": {
+    "id": "blue",
+    "name": "Blue",
+    "type": "static",
+    "price": 10000,
+    "glow": "#0064ff",
+    "tier": "common",
+    "videoUrl": null
+  },
+  "yellow": {
+    "id": "yellow",
+    "name": "Yellow",
+    "type": "static",
+    "price": 10000,
+    "glow": "#ffff00",
+    "tier": "common",
+    "videoUrl": null
+  },
+  "green": {
+    "id": "green",
+    "name": "Green",
+    "type": "static",
+    "price": 10000,
+    "glow": "#00ff00",
+    "tier": "common",
+    "videoUrl": null
+  },
+  "red": {
+    "id": "red",
+    "name": "Red",
+    "type": "static",
+    "price": 10000,
+    "glow": "#ff0000",
+    "tier": "common",
+    "videoUrl": null
+  },
+  "pink": {
+    "id": "pink",
+    "name": "Pink",
+    "type": "static",
+    "price": 12000,
+    "glow": "#ff00ff",
+    "tier": "rare",
+    "videoUrl": null
+  },
+  "purple": {
+    "id": "purple",
+    "name": "Purple",
+    "type": "static",
+    "price": 12000,
+    "glow": "#c800ff",
+    "tier": "rare",
+    "videoUrl": null
+  },
+  "default": {
+    "id": "default",
+    "name": "Default",
+    "type": "static",
+    "price": 0,
+    "glow": "#ffffff",
+    "tier": "common",
+    "videoUrl": null
+  },
+  "girl": {
+    "id": "girl",
+    "name": "Girl",
+    "type": "static",
+    "price": 25000,
+    "glow": "#ff69b4",
+    "tier": "epic",
+    "videoUrl": null
+  },
+  "rainbow": {
+    "id": "rainbow",
+    "name": "Rainbow",
+    "type": "static",
+    "price": 20000,
+    "glow": "#ff00ff",
+    "tier": "epic",
+    "videoUrl": null
+  },
+  "nebula": {
+    "id": "nebula",
+    "name": "Nebula",
+    "type": "static",
+    "price": 30000,
+    "glow": "#aa00ff",
+    "tier": "epic",
+    "videoUrl": null
+  },
+  "space": {
+    "id": "space",
+    "name": "Space",
+    "type": "static",
+    "price": 35000,
+    "glow": "#aa00ff",
+    "tier": "epic",
+    "videoUrl": null
+  },
+  "lightyear": {
+    "id": "lightyear",
+    "name": "Light Year",
+    "type": "static",
+    "price": 40000,
+    "glow": "#00c8ff",
+    "tier": "legendary",
+    "videoUrl": null
+  },
+  "ice": {
+    "id": "ice",
+    "name": "Ice",
+    "type": "static",
+    "price": 10000,
+    "glow": "#00d4ff",
+    "tier": "rare",
+    "videoUrl": null
+  },
+  "gold": {
+    "id": "gold",
+    "name": "Gold",
+    "type": "static",
+    "price": 25000,
+    "glow": "#ffd700",
+    "tier": "epic",
+    "videoUrl": null
+  },
+  "diamond": {
+    "id": "diamond",
+    "name": "Diamond",
+    "type": "static",
+    "price": 50000,
+    "glow": "#b4ffff",
+    "tier": "legendary",
+    "videoUrl": null
+  },
+  "nightcity": {
+    "id": "nightcity",
+    "name": "Night City",
+    "type": "static",
+    "price": 18000,
+    "glow": "#ff0066",
+    "tier": "rare",
+    "videoUrl": null
+  },
+  "overlays": [
+    "jail_bars",
+    "wanted_frame",
+    "mugshot_lines",
+    "blood_splatter",
+    "explosion",
+    "steal_success",
+    "jail_caught"
+  ]
+}
